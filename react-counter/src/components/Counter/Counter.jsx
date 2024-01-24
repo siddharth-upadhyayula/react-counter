d922db3ac920bf182b5ec3c990375f9c1941c78e
Sai Siddharth Upadhyayula <usaisiddarth@gmail.com>
2024-01-24 11:10:58 -0500

    Implement +1 counter

---
+++ react-counter/src/components/Counter/Counter.jsx
@@ -0,0 +1,22 @@
+import './Counter.css'
+import { useState } from 'react';
+
+export default function Counter(){
+
+    const [count, setCount] = useState(0);
+    
+    function incrementCounterFunction() {
+        setCount(count+1)
+        console.log('increment clicked')
+    }
+    
+    return (
+        <div className="Counter">
+            <span className="count">{count}</span>
+            <div>
+                <button className="counterButton" onClick={incrementCounterFunction}>+1</button>
+            </div>
+        </div>
+    ) 
+
+} 
