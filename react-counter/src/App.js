5aef0ced5eca97cf26186366173a76723254b1d3
Sai Siddharth Upadhyayula <usaisiddarth@gmail.com>
2024-01-24 20:22:55 -0500

    Implement reset button, state up and refactoring the code

--- react-counter/src/App.js
+++ react-counter/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import Counter from './components/Counter/Counter';
+import CounterButton from './components/Counter/Counter';
 
 function App() {
   return (
     <div className="App">
-      <Counter />
+      <CounterButton />
     </div>
   );
 }
--- react-counter/src/components/Counter/Counter.jsx
+++ react-counter/src/components/Counter/Counter.jsx
@@ -1,22 +1,38 @@
+import { useState } from 'react'
 import './Counter.css'
-import { useState } from 'react';
+import CounterButton from '../CounterButton'
 
-export default function Counter(){
-
-    const [count, setCount] = useState(0);
+export default function Counter() {
     
-    function incrementCounterFunction() {
-        setCount(count+1)
-        console.log('increment clicked')
+    const [count, setCount] = useState(0)
+
+    function incrementCounterParentFunction(by) {
+        setCount(count + by)
     }
-    
-    return (
-        <div className="Counter">
-            <span className="count">{count}</span>
-            <div>
-                <button className="counterButton" onClick={incrementCounterFunction}>+1</button>
-            </div>
-        </div>
-    ) 
 
-} 
+    function decrementCounterParentFunction(by) {
+        setCount(count - by)
+    }
+
+    function resetCounter() {
+        setCount(0)
+    }
+
+    return (
+        <>
+            <span className="totalCount">{count}</span>
+            <CounterButton by={1} 
+                incrementMethod={incrementCounterParentFunction} 
+                decrementMethod={decrementCounterParentFunction}/>
+            <CounterButton by={2} 
+                incrementMethod={incrementCounterParentFunction}
+                decrementMethod={decrementCounterParentFunction}/>
+            <CounterButton by={5} 
+                incrementMethod={incrementCounterParentFunction}
+                decrementMethod={decrementCounterParentFunction}/>
+            <button className="resetButton" 
+                        onClick={resetCounter}
+                >Reset</button>
+        </>
+    )
+}
